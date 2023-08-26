983d3674a5d5934d6471025ea920e782de922ad5
IrinaGM <irina.golionko@gmail.com>
2023-08-26 15:43:38 +0000

    solution for exercise 1 & 2

--- exercises/exercise_1.rb
+++ exercises/exercise_1.rb
@@ -1,6 +1,13 @@
 require_relative '../setup'
+require_relative '../lib/store.rb'
 
 puts "Exercise 1"
 puts "----------"
 
 # Your code goes below here ...
+Store.create(name: "Burnaby", annual_revenue: "300000", mens_apparel: true, womens_apparel: true)
+Store.create(name: "Richmond", annual_revenue: "1260000", mens_apparel: false, womens_apparel: true)
+Store.create(name: "Gastown", annual_revenue: "190000", mens_apparel: true, womens_apparel: false)
+
+
+puts "Count of stores in DB: #{Store.count(:name)}"
