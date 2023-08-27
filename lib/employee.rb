2696f6bf54e7e8b0d8624f5ca68aff679164b09f
IrinaGM <irina.golionko@gmail.com>
2023-08-27 18:00:07 +0000

    add validations to employee and store, add solution to exercise 7

--- lib/employee.rb
+++ lib/employee.rb
@@ -1,3 +1,9 @@
 class Employee < ActiveRecord::Base
   belongs_to :store
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+  validates :store_id, presence: true
+  validates :hourly_rate, numericality: { only_integer: true }
+  validates :hourly_rate, comparison: { greater_than: 40 }
+  validates :hourly_rate, comparison: { less_than: 200 }
 end
--- lib/store.rb
+++ lib/store.rb
@@ -1,3 +1,13 @@
 class Store < ActiveRecord::Base
   has_many :employees
+  validates :name, length: { minimum: 3,
+    too_short: "must be minimum of %{count}" }
+  validates :annual_revenue, numericality: { only_integer: true, greater_than_or_equal_to: 0}
+  validate :carry_apperal
+
+  def carry_apperal
+    if womens_apparel == nil && mens_apparel == nil || womens_apparel == false && mens_apparel == false
+      errors.add(:base, "Store must carry woman and, or mans apparel")
+    end
+  end
 end
